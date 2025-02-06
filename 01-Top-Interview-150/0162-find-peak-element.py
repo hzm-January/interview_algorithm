ad9d2a7fddd10337528868ba4c52630b2f38110c
hzm-January <hzm_dream@163.com>
2025-02-06 15:07:16 +0800

    binary search

---
+++ 01-Top-Interview-150/0162-find-peak-element.py
@@ -0,0 +1,88 @@
+import math
+import random
+
+
+def findPeakElement2(nums: list[int]) -> int:
+    """
+        爬坡
+    :param nums:
+    :return:
+    """
+    n = len(nums)
+    idx = random.randint(0, n - 1)
+
+    def get(index: int):
+        if index == -1 or index == n:
+            return -math.inf
+        return nums[index]
+
+    while not get(idx - 1) < get(idx) > get(idx + 1):
+        if get(idx) > get(idx + 1): # 爬坡，向上，也可以向下
+            idx = idx - 1
+        else:
+            idx = idx + 1
+    return idx
+
+def findPeakElement2_2(nums: list[int]) -> int:
+    """
+        爬坡
+    :param nums:
+    :return:
+    """
+    n = len(nums)
+    idx = random.randint(0, n - 1)
+
+    def get(index: int):
+        if index == -1 or index == n:
+            return -math.inf
+        return nums[index]
+
+    while not get(idx - 1) < get(idx) > get(idx + 1):
+        if get(idx) < get(idx - 1): # 爬坡，向上，也可以向下
+            idx = idx - 1
+        else:
+            idx = idx + 1
+    return idx
+
+
+def findPeakElement3(nums: list[int]) -> int:
+    """
+        二分查找
+    """
+    nums = [-math.inf] + nums + [-math.inf]
+    left, right = 1, len(nums) - 2
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
+            return mid - 1
+        if nums[mid] < nums[mid - 1]:  # 这里使用nums[mid-1]判断，也可使用nums[mid+1]判断
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
+
+def findPeakElement3_2(nums: list[int]) -> int:
+    """
+        二分查找
+    """
+    nums = [-math.inf] + nums + [-math.inf]
+    left, right = 1, len(nums) - 2
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
+            return mid - 1
+        if nums[mid] < nums[mid + 1]:  # 这里使用nums[mid-1]判断，也可使用nums[mid+1]判断
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+if __name__ == "__main__":
+    # nums = [1, 2, 3, 1]
+    nums = [1, 2, 1, 3, 5, 6, 4]
+    # nums=[1]
+    # ans = findPeakElement3(nums)
+    ans = findPeakElement2(nums)
+    print(ans)
