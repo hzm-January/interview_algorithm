1cca34b92ddab989c668039455b6f256a29bb5f5
hzm-January <hzm_dream@163.com>
2025-02-15 11:51:26 +0800

    整理

--- 89-dynamic-programming/2-leetcode-LCR-0081-combination-sum.py
+++
@@ -1,9 +0,0 @@
-def combinationSum(candidates: list[int], target: int) -> list[list[int]]:
-
-    return None
-
-
-if __name__ == "__main__":
-    candidates, target = [2,3,6,7], 7
-    ans = combinationSum(candidates, target)
-    print(ans)
