5826c65f640c9059708b7632c74e8f851ea972be
hzm-January <hzm_dream@163.com>
2025-02-24 11:38:13 +0800

    回溯 错误写法记录

--- 87-backtracking/1-leetcode-1005-maximize-sum-of-array-after-k-negations.py
+++ 87-backtracking/1-leetcode-1005-maximize-sum-of-array-after-k-negations.py
@@ -3,24 +3,29 @@ from typing import List
 
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        """
+            错误代码 超时
+            回溯写法 超时
+            超时测试样例：[-1,-3,8,-6,-9,2,4,0]
+        """
         n = len(nums)
         path, ans = nums.copy(), []
         maxSum = [0]
-        self.backtracking(nums, k, 0, path, ans, maxSum, 0)
+        # self.backtracking(nums, k, 0, path, ans, maxSum, 0)
+        def backtracking(t, curSum):
+            """ 该写法 超时 """
+            if t == k:  # 1 终止条件
+                if maxSum[0] < curSum:
+                    maxSum[0] = curSum
+                    ans = path.copy()
+                return
+            for i in range(n):
+                path[i] = -path[i]
+                backtracking(t+1, curSum - (-path[i]) + path[i])
+                path[i] = -path[i]
+        backtracking(0, sum(nums))
         return maxSum[0]
 
-    def backtracking(self, nums: List[int], k: int, t: int, path: List[int], ans: List[List[int]],
-                     maxSum: list[int], curSum:int) -> None:
-        """ 该写法 超时 """
-        if t == k:  # 1 终止条件
-            if maxSum[0] < curSum:
-                maxSum[0] = curSum
-                ans = path.copy()
-            return
-        for i in range(len(nums)):
-            path[i] = -path[i]
-            self.backtracking(nums, k, t + 1, path, ans, maxSum, curSum-(-path[i])+path[i])
-            path[i] = -path[i]
 
 
 if __name__ == '__main__':
