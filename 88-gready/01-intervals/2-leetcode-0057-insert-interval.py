37c305ce9eb23ac35452499dc7cc4b697e45e56b
hzm-January <hzm_dream@163.com>
2025-02-27 19:28:37 +0800

    greedy

--- 88-gready/01-intervals/2-leetcode-0057-insert-interval.py
+++ 88-gready/01-intervals/2-leetcode-0057-insert-interval.py
@@ -5,22 +5,41 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         n = len(intervals)
         pre = 0
-        index = -1
-        for i in range(n - 1, -1, -1):
-            if intervals[i][1] < newInterval[0]:  # 第一个右端点小于 newInterval左端点
+        index = n
+        ans = []
+        for i in range(n):
+            if intervals[i][1] > newInterval[0]:  # 第一个右端点大于 newInterval左端点
                 index = i
                 break
-        if index == n - 1:
+            ans.append(intervals[i])
+
+        # Case 1 newInterval左端点比最后一个区间的右端点大，直接添加在末尾
+        if index == n:
             intervals.append(newInterval)
             return intervals
+        # Case 2 中间插入
+        if intervals[index][1] > newInterval[1]:  # 被包含
+            return intervals
+        if intervals[index][0] > newInterval[1]:  # 插入 无重叠
+            intervals[index - 1:index - 1] = [newInterval]
+            return intervals
 
-        # 与 index+1 位置区间合并
-        index = index + 1
+        # 与 index 位置区间合并
         intervals[index] = [min(newInterval[0], intervals[index][0]), max(newInterval[1], intervals[index][1])]
-        i = index
-        while i < n:
-            if intervals[i-1][1] > intervals[i][0]:
+        ans.append(intervals[index])
+
+        for i in range(index + 1, n):
+            if ans[-1][1] > intervals[i][0]:  # 有重叠
                 # 合并
-                intervals[i - 1][1]=max(intervals[i-1][1], intervals[i][1])
+                ans[-1][1] = max(ans[-1][1], intervals[i][1])
+            else:
+                ans.append(intervals[i])
+
+        return ans
 
 
+if __name__ == '__main__':
+    # intervals = [[1, 3], [6, 9]]
+    # newInterval = [2, 5]
+    intervals, newInterval = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]
+    print(Solution().insert(intervals, newInterval))
