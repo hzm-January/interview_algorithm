b2917f4211a13f5bce28351283e961ca07dbba15
hzm-January <hzm_dream@163.com>
2025-02-27 17:58:19 +0800

    greedy

--- 88-gready/2-leetcode-0378-monotone-increasing-digits.py
+++ 88-gready/2-leetcode-0378-monotone-increasing-digits.py
@@ -19,13 +19,16 @@ class Solution:
             tmp -= 1
 
     def monotoneIncreasingDigits2(self, n: int) -> int:
-        """ 贪心 数字处理 """
+        """
+            贪心 数字处理
+            思路：两个两个比，两个数字中，前一个比后一个大，这前一个减1，后一个变为9
+        """
         ns = [int(c) for c in str(n)]
         flag = len(ns)
         for i in range(len(ns) - 1, 0, -1):
             if ns[i - 1] > ns[i]:
                 ns[i - 1] -= 1
-                flag = i
+                flag = i  # 记录i，而不是直接赋值9，测试样例：1000，如果是直接赋值9，运行结束后诶900
         if flag < len(ns): ns[flag:] = [9] * (len(ns) - flag)
         return int(''.join(map(str, ns)))
 
@@ -38,7 +41,8 @@ class Solution:
             if int(ns[i - 1]) > int(ns[i]):
                 ns[i - 1] = str(int(ns[i - 1]) - 1)
                 flag = i
-        if flag < len(ns): ns[flag:] = ['9'] * (len(ns) - flag)
+        for i in range(flag, len(ns)):
+            ns[i] = '9'
         return int(''.join(ns))
 
 
