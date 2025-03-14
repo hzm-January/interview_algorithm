94d804f5dbb114786fbdb890cc9f737f3cc64f15
hzm-January <hzm_dream@163.com>
2025-03-14 09:50:12 +0800

    dp

--- 89-dynamic-programming/00-base-dp/2-leetcode-096-unique-binary-search-tree.py
+++ 89-dynamic-programming/00-base-dp/2-leetcode-096-unique-binary-search-tree.py
@@ -1,18 +1,26 @@
 def numTrees(n: int) -> int:
+    """
+        1 dp数组含义：由i个节点组成的1~n互不相同的二叉搜索树有dp[i]种
+        2 递推公式：dp[i]+=dp[i-1]*dp[j-i]
+        3 dp数组初始化：dp[0]=1 dp[i]=0
+        4 遍历顺序：从前到后
+        5 打印：
+    """
     dp = [0] * (n + 1)
     dp[0] = 1
-    for i in range(1, n + 1):
-        for j in range(1, i + 1):
+    for i in range(1, n + 1):  # 有i个节点
+        for j in range(1, i + 1):  # 分别以j作为根节点
             dp[i] += dp[j - 1] * dp[i - j]
-    return dp[n]
+
+    return dp[-1]
 
 
 def numTrees_2(n: int) -> int:
     """
-     卡特兰数
+        卡特兰数
     """
     C = 1
-    for i in range(0, n):
+    for i in range(0, n):  # 根据i=n-1计算的Cn
         C = C * 2 * (2 * i + 1) / (i + 2)
     return int(C)
 
